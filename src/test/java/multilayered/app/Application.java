c95b3e5aac81cc1ca2e6357deddc1797cacfab86
androidemotions <sparrow@mail.ua>
2017-11-08 19:24:19 +0200

    Add fluent style to page description

--- src/test/java/multilayered/app/Application.java
+++ src/test/java/multilayered/app/Application.java
@@ -5,19 +5,13 @@ import multilayered.data.Customer;
 import multilayered.pages.AdminPanelLoginPage;
 import multilayered.pages.CustomerListPage;
 import multilayered.pages.RegistrationPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.Set;
-
-import static java.util.stream.Collectors.toSet;
 
 public class Application {
 
 
-    private WebDriverWait wait;
     private WebDriver driver;
 
     private RegistrationPage registrationPage;
@@ -27,7 +21,6 @@ public class Application {
     public Application() {
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
         registrationPage = new RegistrationPage(driver);
         adminPanelLoginPage = new AdminPanelLoginPage(driver);
         customerListPage = new CustomerListPage(driver);
@@ -54,18 +47,10 @@ public class Application {
     }
 
     public Set<String> getCustomerIds() {
-        driver.get("http://192.168.56.101/litecart/admin");
-        if (driver.findElements(By.id("box-login")).size() > 0) {
-            driver.findElement(By.name("username")).sendKeys("admin");
-            driver.findElement(By.name("password")).sendKeys("admin");
-            driver.findElement(By.name("login")).click();
-            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
+        if (adminPanelLoginPage.open().isOnThisPage()) {
+            adminPanelLoginPage.enterUsername("admin").enterPassword("admin").submitLogin();
         }
 
-        driver.get("http://192.168.56.101/litecart/admin/?app=customers&doc=customers");
-        return driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
-                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
-                .collect(toSet());
+        return customerListPage.open().getCustomerIds();
     }
-
 }
