6a5d8c0506f7de8fe1ffc2d87cee6bbc3f7c1d8f
androidemotions <sparrow@mail.ua>
2017-11-08 18:56:24 +0200

    Add Data Provider to the test

---
+++ src/test/java/multilayered/tests/DataProviders.java
@@ -0,0 +1,20 @@
+package multilayered.tests;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import multilayered.data.Customer;
+
+public class DataProviders {
+
+    @DataProvider
+    public static Object[][] validCustomers() {
+        return new Object[][]{
+                {Customer.newEntity()
+                        .withFirstname("Adam").withLastname("Smith").withPhone("+0123456789")
+                        .withAddress("Hidden Place").withPostcode("12345").withCity("New City")
+                        .withCountry("US").withZone("KS")
+                        .withEmail("adam" + System.currentTimeMillis() + "@smith.me")
+                        .withPassword("qwerty").build()},
+                /* ... */
+        };
+    }
+}
